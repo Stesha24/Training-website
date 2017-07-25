550111762dd87669b69b451625be462b4f7bdef5
Anastasia Kvashnina <anastasia.kv97@gmail.com>
2017-07-25 11:53:43 +0600

    contacts page

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -3,13 +3,13 @@ var images = ["url(../images/cover.jpg)", "url(../images/cover-about.jpg)"];
 var texts = ["Fully responsive website mockup PSD template. Free for commercial &amp; presonal use.  Portfolio &amp; Contact Pages included.", "azazaz fskjfla jslj woeiqp m,mc a;s al oa dals kaflma ajd ; dw mlm aslkd wjdk jw."]
 
 var i = 0;
-$("header").css("background-image", images[i]);
+$(".header__main").css("background-image", images[i]);
 $(".index__right_text").html(texts[i]);
 $( ".slider__button_right" ).click(function() {
     i++;
     if(i==2) i=0;
     console.log(i);
-  $("header").css("background-image", images[i]);
+  $(".header__main").css("background-image", images[i]);
     $(".index__right_text").html(texts[i]);
 });
 
@@ -17,6 +17,6 @@ $( ".slider__button_left" ).click(function() {
     i--;
     if(i==-1) i=1;
     console.log(i);
-  $("header").css("background-image", images[i]);
+  $(".header__main").css("background-image", images[i]);
     $(".index__right_text").html(texts[i]);
 });
